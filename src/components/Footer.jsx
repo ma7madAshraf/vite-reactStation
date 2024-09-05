5b648b0f31cd3c575ee0ab3d376879af0588e51c
ma7madAshraf <ma7mad.ashraf@gmail.com>
2024-09-05 10:27:25 +0300

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,12 +8,12 @@ const Footer = () => {
     theme === "synthwave" ? " text-neutral-content" : " text-slate-900";
   useEffect(() => {}, [text]);
   return (
-    <section className="my-align flex justify-between items-center fixed bottom-0 w-full h-7 bg-base-200 text-primary-content z-50 font-semibold text-sm">
+    <section className=" mx-auto  px-4 sm:px-8 flex justify-between items-center fixed bottom-0 w-full h-7 bg-base-200 text-primary-content z-50 font-semibold text-sm">
       <div className={`left  flex items-center text-left  ${text}`}>
         Designed by
         <img alt="logo" src={mLogo} className="w-16 md:w-20" />
       </div>
-      <div className={`right text-right flex items-end ${text} `}>
+      <div className={`right text-right flex items-end  ${text} `}>
         copyright
         <span className="year text-center font-semibold ml-1 text-primary ">
           &copy;{new Date().getFullYear()}
